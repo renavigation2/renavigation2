7e70491edac9d482327b211b75a4dfcf13095c70
Gabriel Bull <me@gabrielbull.com>
2020-07-29 10:40:11 -0400

    fix(modals): Use RCTKeyWindow instead of extending UIViewController to present modals

--- examples/RenavigationExample/src/index.tsx
+++ examples/RenavigationExample/src/index.tsx
@@ -4,7 +4,6 @@ import {
   View,
   Text,
   ScrollView,
-  StatusBar,
   SafeAreaView,
   TouchableOpacity
 } from 'react-native'
@@ -14,19 +13,8 @@ import {
   Route,
   useModals,
   useSetModalDismissible,
-  useModalWillAppearEffect,
-  useModalDidAppearEffect,
-  useModalWillDisappearEffect,
-  useModalDidDisappearEffect,
   useModal
 } from '@renavigation2/modals'
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions
-} from 'react-native/Libraries/NewAppScreen'
 
 const App = () => {
   return (
@@ -44,20 +32,8 @@ const Modal = () => {
   const { dismiss } = useModal()
   const setDismissible = useSetModalDismissible()
   useEffect(() => {
-    //setDismissible(false)
+    setDismissible(false)
   }, [setDismissible])
-  useModalWillAppearEffect(() => {
-    console.log('will appear')
-  })
-  useModalDidAppearEffect(() => {
-    console.log('did appear')
-  })
-  useModalWillDisappearEffect(() => {
-    console.log('will disappear')
-  })
-  useModalDidDisappearEffect(() => {
-    console.log('did disappear')
-  })
   return (
     <View style={{ flex: 1, backgroundColor: 'blue' }}>
       <TouchableOpacity onPress={dismiss}>
@@ -68,46 +44,14 @@ const Modal = () => {
 }
 
 const Hello: React.FC = () => {
-  console.log('render hello')
   return (
     <>
-      <StatusBar barStyle="dark-content" />
-
       <SafeAreaView>
         <ScrollView
           contentInsetAdjustmentBehavior="automatic"
           style={styles.scrollView}
         >
-          <Header />
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this
-                screen and then come back to see your edits.
-              </Text>
-            </View>
-            <Actions />
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
-          </View>
+          <Actions />
         </ScrollView>
       </SafeAreaView>
     </>
@@ -130,36 +74,29 @@ const Actions: React.FC = () => {
 }
 
 const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter
-  },
+  scrollView: {},
   engine: {
     position: 'absolute',
     right: 0
   },
-  body: {
-    backgroundColor: Colors.white
-  },
+  body: {},
   sectionContainer: {
     marginTop: 32,
     paddingHorizontal: 24
   },
   sectionTitle: {
     fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black
+    fontWeight: '600'
   },
   sectionDescription: {
     marginTop: 8,
     fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark
+    fontWeight: '400'
   },
   highlight: {
     fontWeight: '700'
   },
   footer: {
-    color: Colors.dark,
     fontSize: 12,
     fontWeight: '600',
     padding: 4,
--- packages/modals/ios/RenavigationModals-Bridging-Header.h
+++ packages/modals/ios/RenavigationModals-Bridging-Header.h
@@ -2,7 +2,7 @@
 #import <React/RCTEventEmitter.h>
 #import <React/RCTTouchHandler.h>
 #import <React/RCTUIManager.h>
+#import <React/RCTUtils.h>
 #import <React/RCTView.h>
 #import <React/RCTViewManager.h>
 #import <React/UIView+React.h>
-#import <UIViewController+TopMostController.h>
