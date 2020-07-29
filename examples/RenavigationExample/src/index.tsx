bbb93588b246633a3019f00fc7f9dc1401827478
Gabriel Bull <me@gabrielbull.com>
2020-07-29 09:18:52 -0400

    chore: Updated dependencies

--- examples/RenavigationExample/src/index.tsx
+++ examples/RenavigationExample/src/index.tsx
@@ -1,34 +1,84 @@
-import React from 'react';
+import React, { useCallback, useEffect } from 'react'
 import {
-  SafeAreaView,
   StyleSheet,
-  ScrollView,
   View,
   Text,
+  ScrollView,
   StatusBar,
+  SafeAreaView,
+  TouchableOpacity
 } from 'react-native'
+import {
+  ModalsRouter,
+  ModalsRoutes,
+  Route,
+  useModals,
+  useSetModalDismissible,
+  useModalWillAppearEffect,
+  useModalDidAppearEffect,
+  useModalWillDisappearEffect,
+  useModalDidDisappearEffect,
+  useModal
+} from '@renavigation2/modals'
 import {
   Header,
   LearnMoreLinks,
   Colors,
   DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+  ReloadInstructions
+} from 'react-native/Libraries/NewAppScreen'
 
 const App = () => {
   return (
+    <ModalsRouter>
+      <Hello />
+
+      <ModalsRoutes>
+        <Route path="*" element={<Modal />} />
+      </ModalsRoutes>
+    </ModalsRouter>
+  )
+}
+
+const Modal = () => {
+  const { dismiss } = useModal()
+  const setDismissible = useSetModalDismissible()
+  useEffect(() => {
+    //setDismissible(false)
+  }, [setDismissible])
+  useModalWillAppearEffect(() => {
+    console.log('will appear')
+  })
+  useModalDidAppearEffect(() => {
+    console.log('did appear')
+  })
+  useModalWillDisappearEffect(() => {
+    console.log('will disappear')
+  })
+  useModalDidDisappearEffect(() => {
+    console.log('did disappear')
+  })
+  return (
+    <View style={{ flex: 1, backgroundColor: 'blue' }}>
+      <TouchableOpacity onPress={dismiss}>
+        <Text>Close</Text>
+      </TouchableOpacity>
+    </View>
+  )
+}
+
+const Hello: React.FC = () => {
+  console.log('render hello')
+  return (
     <>
       <StatusBar barStyle="dark-content" />
+
       <SafeAreaView>
         <ScrollView
           contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
+          style={styles.scrollView}
+        >
           <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
-          )}
           <View style={styles.body}>
             <View style={styles.sectionContainer}>
               <Text style={styles.sectionTitle}>Step One</Text>
@@ -37,6 +87,7 @@ const App = () => {
                 screen and then come back to see your edits.
               </Text>
             </View>
+            <Actions />
             <View style={styles.sectionContainer}>
               <Text style={styles.sectionTitle}>See Your Changes</Text>
               <Text style={styles.sectionDescription}>
@@ -60,37 +111,52 @@ const App = () => {
         </ScrollView>
       </SafeAreaView>
     </>
-  );
-};
+  )
+}
+
+const Actions: React.FC = () => {
+  const { presentModal } = useModals()
+  const onPress = useCallback(() => {
+    presentModal('/hello')
+  }, [presentModal])
+
+  return (
+    <TouchableOpacity onPress={onPress}>
+      <View>
+        <Text>Open modal</Text>
+      </View>
+    </TouchableOpacity>
+  )
+}
 
 const styles = StyleSheet.create({
   scrollView: {
-    backgroundColor: Colors.lighter,
+    backgroundColor: Colors.lighter
   },
   engine: {
     position: 'absolute',
-    right: 0,
+    right: 0
   },
   body: {
-    backgroundColor: Colors.white,
+    backgroundColor: Colors.white
   },
   sectionContainer: {
     marginTop: 32,
-    paddingHorizontal: 24,
+    paddingHorizontal: 24
   },
   sectionTitle: {
     fontSize: 24,
     fontWeight: '600',
-    color: Colors.black,
+    color: Colors.black
   },
   sectionDescription: {
     marginTop: 8,
     fontSize: 18,
     fontWeight: '400',
-    color: Colors.dark,
+    color: Colors.dark
   },
   highlight: {
-    fontWeight: '700',
+    fontWeight: '700'
   },
   footer: {
     color: Colors.dark,
@@ -98,8 +164,8 @@ const styles = StyleSheet.create({
     fontWeight: '600',
     padding: 4,
     paddingRight: 12,
-    textAlign: 'right',
-  },
-});
+    textAlign: 'right'
+  }
+})
 
-export default App;
+export default App
--- scripts/update-dependencies.sh
+++ scripts/update-dependencies.sh
@@ -1,17 +1,15 @@
 #!/bin/sh
 
 update () {
-    npx ncu --packageFile "$1/package.json" -u
-    npx ncu --packageFile "$1/package.json" -u --dep dev
+    npx ncu --packageFile "$1/package.json" -u -x path-to-regexp
+    npx ncu --packageFile "$1/package.json" -u --dep dev -x path-to-regexp
 }
 
-yarn workspaces info | awk '/location/ {print $2}' | while read package; do
-    package="${package//\"}"
-    package="${package//,}"
+yarn workspaces list --json | while read package; do
+    package="${package/\{\"location\":\"/}"
+    package=$(sed "s/\",\"name\":.*//g" <<< "$package")
     update "$package"
 done
-
-update "."
 
 yarn clean -y
 rm yarn.lock
