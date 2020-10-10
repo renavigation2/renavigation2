f14f7c594c18717c07cef8be355c4ef8a1ce550a
Gabriel Bull <me@gabrielbull.com>
2020-10-10 18:01:06 -0400

    feat(modals): Added support for style on Modal component

--- packages/modals/src/components/Modal.tsx
+++ packages/modals/src/components/Modal.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
+import { StyleSheet, View, ViewProps } from 'react-native'
 import { ModalConfig } from '../native/ModalConfig'
 import { ModalPresentationStyleValue } from '../native/ModalPresentationStyle'
 import { ModalTransitionStyleValue } from '../native/ModalTransitionStyle'
 import { StatusBarAnimationValue } from '../native/StatusBarAnimation'
 import { StatusBarStyleValue } from '../native/StatusBarStyle'
 
-export interface ModalProps {
+export interface ModalProps extends ViewProps {
   animated?: boolean
   isModalInPresentation?: boolean
   modalPresentationCapturesStatusBarAppearance?: boolean
@@ -16,11 +17,36 @@ export interface ModalProps {
   prefersStatusBarHidden?: boolean
 }
 
-export const Modal: React.FC<ModalProps> = ({ children, ...props }) => {
+export const Modal: React.FC<ModalProps> = ({
+  animated,
+  isModalInPresentation,
+  modalPresentationCapturesStatusBarAppearance,
+  modalPresentationStyle,
+  modalTransitionStyle,
+  preferredStatusBarUpdateAnimation,
+  preferredStatusBarStyle,
+  prefersStatusBarHidden,
+  children,
+  style,
+  ...props
+}) => {
   return (
     <>
-      <ModalConfig {...props} />
-      {children}
+      <ModalConfig
+        animated={animated}
+        isModalInPresentation={isModalInPresentation}
+        modalPresentationCapturesStatusBarAppearance={
+          modalPresentationCapturesStatusBarAppearance
+        }
+        modalPresentationStyle={modalPresentationStyle}
+        modalTransitionStyle={modalTransitionStyle}
+        preferredStatusBarUpdateAnimation={preferredStatusBarUpdateAnimation}
+        preferredStatusBarStyle={preferredStatusBarStyle}
+        prefersStatusBarHidden={prefersStatusBarHidden}
+      />
+      <View style={[StyleSheet.absoluteFill, style]} {...props}>
+        {children}
+      </View>
     </>
   )
 }
