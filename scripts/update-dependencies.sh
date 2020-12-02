5b1cb208ea2005ade9cfe3830ebfaaaa01400bc8
Gabriel Bull <me@gabrielbull.com>
2020-12-02 11:26:21 -0500

    chore: Updated dependencies

--- scripts/update-dependencies.sh
+++ scripts/update-dependencies.sh
@@ -5,8 +5,6 @@ update () {
     npx ncu --packageFile "$1/package.json" -u --dep dev -x path-to-regexp
 }
 
-yarn set version latest
-
 yarn workspaces list --json | while read package; do
     package="${package/\{\"location\":\"/}"
     package=$(sed "s/\",\"name\":.*//g" <<< "$package")
@@ -16,3 +14,5 @@ done
 yarn clean -y
 rm yarn.lock
 yarn install
+
+yarn set version latest
