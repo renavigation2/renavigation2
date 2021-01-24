594260d713dfce8fbe4e12cf1b6e8b27d38b9cdb
Gabriel Bull <me@gabrielbull.com>
2021-01-23 23:06:39 -0500

    fix(navigation): Standardized text style options

--- packages/navigation/src/ios/typings/TextStyle.ts
+++ packages/navigation/src/ios/typings/TextStyle.ts
@@ -43,7 +43,7 @@ export interface TextStyle {
   firstLineHeadIndent?: number
   headIndent?: number
   tailIndent?: number
-  lineBreakMode?: 'clip' | 'head' | 'tail' | 'middle' | 'wordWrapping'
+  lineBreakMode?: 'clip' | 'head' | 'tail' | 'middle' | 'word-wrapping'
   minimumLineHeight?: number
   maximumLineHeight?: number
   lineHeightMultiple?: number
@@ -51,5 +51,5 @@ export interface TextStyle {
   hyphenationFactor?: number
   defaultTabInterval?: number
   allowsDefaultTighteningForTruncation?: boolean
-  lineBreakStrategy?: 'pushOut' | 'hangulWordPriority' | 'standard'
+  lineBreakStrategy?: 'push-out' | 'hangul-word-priority' | 'standard'
 }
